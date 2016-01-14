02a6e1b87f1c97c7fa4b1adc281786200056f735
Tritia Timmins <tritiatimmins@Tritias-MacBook-Pro.local>
2016-01-14 00:33:20 -0800

    starts question 5. finishes question 4

--- assessment.js
+++ assessment.js
@@ -21,5 +21,31 @@ var javaScriptArray = ['j', 'a', 'v', 'a'];
 //JS console
 
 for (var i = 0; i < string.length; i++) {
-	console.log(i);
+	console.log(string[i]);
+}
+
+//3. refractor problem 2 using a while loop
+
+var j = 0;
+while(j <= string.length) {
+	j++;
+	console.log(string[j]);	
+}
+
+//some reason it says 'avascript is awesome', and i think this is bc j starts at 0.
+
+//4.use a certain kind of notation to add values to myProfileObject
+
+//I used bracket notation because when I used dot notation, location and name were showing up as keywords, so I thought that this would be a better way.
+
+myProfileObject["school"] = "Telegraph Prep";
+myProfileObject["location"] = "Berkeley, California";
+myProfileObject["name"] = "Tritia Timmins";
+
+console.log(myProfileObject);
+
+//5. create a function that takes an object and a name as parameters. 
+
+var specialReturnFunc = function(object, name) {
+	return object;
 }
